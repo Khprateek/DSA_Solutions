f5c65c1aec69c1f4206125a293239ae1037be080
Prateek kharwar <99474420+Khprateek@users.noreply.github.com>
2024-08-17 16:38:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums) {
+        int n = nums.size();
+        long long int product =1;
+        int Zcnt =0;
+        for(int i =0;i<n;i++){
+            if(nums[i] == 0){
+                Zcnt++;
+            }
+        }
+        if(Zcnt <1){
+            for(int i = 0; i<n;i++){
+                product  = product*nums[i];
+            }
+            for(int i =0;i<n;i++){
+                nums[i] = product/nums[i];
+            }
+        }
+        else if(Zcnt == 1){
+            for(int i = 0; i<n;i++){
+                if(nums[i] == 0){
+                    
+                }
+                else{
+                    product  = product*nums[i];
+                }
+            }
+            for(int i =0;i<n;i++){
+            if(nums[i] == 0){
+                nums[i] = product;
+                }
+            else{
+                nums[i] = 0;
+                }
+            }
+        }
+        else if(Zcnt >1){
+            for(int i = 0; i<n;i++){
+             nums[i] =0;
+            }
+        }
+        return nums;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t; // number of test cases
+    cin >> t;
+    while (t--) {
+        int n; // size of the array
+        cin >> n;
+        vector<long long int> arr(n), vec(n);
+
+        for (int i = 0; i < n; i++) // input the array
+        {
+            cin >> arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr); // function call
+
+        for (int i = 0; i < n; i++) // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

afa3bb4ab274ab6f4fe8a45bab0892ee63666776
Prateek kharwar <99474420+Khprateek@users.noreply.github.com>
2024-09-04 12:45:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    private:
+    int sus(int ind,int pos,vector<vector<int>>& triangle,vector<vector<int>> &dp){
+        if(ind == triangle.size()-1){
+            return triangle[ind][pos];
+        }
+        int mini = INT_MAX;
+        if(dp[ind][pos] != 0){
+            return dp[ind][pos];
+        }
+        for(int i = 0 ;i <= ind; i++){
+            mini = min(mini,triangle[ind][pos] + min(sus(ind+1,pos,triangle,dp),sus(ind+1,pos+1,triangle,dp)));
+        }
+        return dp[ind][pos] = mini;
+    }
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        return sus(0,0,triangle,dp);
+    }
+};

6fb737884182ea6ee157d5e0e56640a3e7d26047
Prateek kharwar <99474420+Khprateek@users.noreply.github.com>
2024-09-04 13:36:08 +0530

    Time: 8 ms (28.01%), Space: 11.7 MB (53.57%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,22 +1,20 @@
+// solution using tabulation
+
 class Solution {
-    private:
-    int sus(int ind,int pos,vector<vector<int>>& triangle,vector<vector<int>> &dp){
-        if(ind == triangle.size()-1){
-            return triangle[ind][pos];
-        }
-        int mini = INT_MAX;
-        if(dp[ind][pos] != -1){
-            return dp[ind][pos];
-        }
-        for(int i = 0 ;i <= ind; i++){
-            mini = min(mini,triangle[ind][pos] + min(sus(ind+1,pos,triangle,dp),sus(ind+1,pos+1,triangle,dp)));
-        }
-        return dp[ind][pos] = mini;
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return sus(0,0,triangle,dp);
+        int n= triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int j = 0;j <n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j = i ; j >= 0; j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down,diag);
+            }
+        }
+        return dp[0][0];
     }
 };

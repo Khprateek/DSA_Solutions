48051c8e2ba7b9b9fb89f66d6b3b75f24c4f93e0
Prateek kharwar <99474420+Khprateek@users.noreply.github.com>
2024-10-06 07:26:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -4,54 +4,55 @@ using namespace std;
 
 // } Driver Code Ends
 
-
+class DisjointSet{
+  vector<int> size,parent;
+  public:
+  DisjointSet(int n){
+      size.resize(n+1,1);//for both 1 based and 0 based.
+      parent.resize(n+1);
+      for(int i = 0; i< n+1;i++){
+          parent[i] = i;
+      }
+  }
+  int FuP(int node){
+      if(parent[node] == node) return node;
+      return parent[node] = FuP(parent[node]);
+  }
+  void UbS(int u, int v){
+      int ulp_u = FuP(u);
+      int ulp_v = FuP(v);
+      if(ulp_u == ulp_v) return;
+      if(size[ulp_u] < size[ulp_v]){
+          size[ulp_v] += size[ulp_u];
+          parent[ulp_u] = parent[ulp_v];
+      }
+      else{
+          size[ulp_u] += size[ulp_v];
+          parent[ulp_v] = parent[ulp_u];
+      }
+  }
+};
 class Solution
 {
-    
-    // PRIMS ALGORITHM//
-    
 	public:
-    int spanningTree(int V, vector<vector<int>> adj[])
+    int spanningTree(int V, vector<pair<int,pair<int,int>>> adj)
     {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        
-        vector<int> vis(V,0);
-        pq.push({0,0});
-        int sum = 0;
-        
-        while(!pq.empty()){
-            
-            auto it = pq.top();
-            pq.pop();
-            
+        DisjointSet ds(V);
+        sort(adj.begin(),adj.end());
+        int mst = 0;
+        for(auto it : adj){
             int wt = it.first;
-            int node = it.second;
-            
-            if(vis[node] == 1) continue;
-            
-            vis[node] = 1;
-            sum += wt;
-            
-            for(auto &itt : adj[node]){
-                
-                int adjwt = itt[1];
-                int adjnode = itt[0];
-                
-                if(!vis[adjnode]){
-                    pq.push({adjwt,adjnode});
-                }
-                
+            int u = it.second.first;
+            int v = it.second.second;
+            if(ds.FuP(u) != ds.FuP(v)){
+                mst += wt;
+                ds.UbS(u,v);
             }
         }
-        return sum;
+        return mst;
     }
+    
 };
-
-
-
-//{ Driver Code Starts.
-
-
 int main()
 {
     int t;
@@ -59,20 +60,13 @@ int main()
     while (t--) {
         int V, E;
         cin >> V >> E;
-        vector<vector<int>> adj[V];
+        vector<pair<int,pair<int,int>>> adj;
         int i=0;
         while (i++<E) {
             int u, v, w;
             cin >> u >> v >> w;
-            vector<int> t1,t2;
-            t1.push_back(v);
-            t1.push_back(w);
-            adj[u].push_back(t1);
-            t2.push_back(u);
-            t2.push_back(w);
-            adj[v].push_back(t2);
+            adj.push_back({w,{u,v}});
         }
-        
         Solution obj;
     	cout << obj.spanningTree(V, adj) << "\n";
     }

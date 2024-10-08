5e4314fc44116613eda06b286139abb313d89e78
Prateek kharwar <99474420+Khprateek@users.noreply.github.com>
2024-10-08 15:11:18 +0530

    Time: 22 ms (49.88%), Space: 18.2 MB (93.82%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,54 @@
-class Solution {
-
-    private:
-    void dfs(int node, vector<int> adj[], vector<int> &vis ){
-        vis[node] =1;
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                dfs(it, adj, vis);
-            }
+class DisjointSet {
+    vector<int> parrent, size;
+public:
+    DisjointSet(int n) {
+        parrent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++) {
+            parrent[i] = i;
         }
     }
-public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int v = isConnected.size();
-        vector<int> adj[v];
-        for(int i =0; i< v; i++){
-            for(int j =0; j< v; j++){
-                if(isConnected[j][i]==1 && j!=i){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
+    int FindP(int x) {
+        if (parrent[x] == x)
+            return x;
+        return parrent[x] = FindP(parrent[x]);
+    }
+    void MkUnion(int u, int v) { 
+        int ulp_u = FindP(u);
+        int ulp_v = FindP(v);
+        if(ulp_u == ulp_v) return ;
+        if(size[ulp_u] < size[ulp_v]){
+            size[ulp_v] += size[ulp_u];
+            parrent[ulp_u] = ulp_v;
         }
-        vector<int> vis(v , 0);
-        int cnt =0;
-        for(int i =0; i<v; i++){
-            if(!vis[i]){
+        else{
+            size[ulp_u] += size[ulp_v];
+            parrent[ulp_v] = ulp_u;
+        }
+    }
+    int countp(){
+        int cnt = 0;
+        for(int i = 0; i< parrent.size();i++){
+            if(FindP(i) == i){
                 cnt++;
-                dfs(i, adj, vis);
             }
         }
         return cnt;
-
+    }
+} ;
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int v = isConnected.size();
+        DisjointSet ds(v);
+        for(int i = 0; i< v; i++){
+            for(int j = 0; j <v; j++){
+                if(isConnected[i][j] == 1){
+                    ds.MkUnion(i,j);
+                }
+            }
+        }
+        int cnt = ds.countp();
+        return cnt;
     }
 };

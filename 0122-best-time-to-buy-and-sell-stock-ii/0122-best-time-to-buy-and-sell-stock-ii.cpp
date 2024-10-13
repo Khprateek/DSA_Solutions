23d73dc6ae94ec46cbc218cc28babe278a396371
Prateek kharwar <99474420+Khprateek@users.noreply.github.com>
2024-10-13 11:55:38 +0530

    Time: 7 ms (40.89%), Space: 18.2 MB (18.36%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -15,7 +15,18 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int> (2,-1));
-        return sus(0,1,n,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int> (2,0));
+        dp[n][0] = dp[n][1] = 0;
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int bb = 0; bb < 2; bb++){
+                if(bb == 1){
+                    dp[ind][bb] = max(-prices[ind] + dp[ind+1][0],dp[ind+1][1]);
+                }
+                else{
+                    dp[ind][bb] = max(prices[ind] + dp[ind+1][1],dp[ind+1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };

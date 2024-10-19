3a27621b51c15c3a931f4170a3d37295cee664f4
Prateek kharwar <99474420+Khprateek@users.noreply.github.com>
2024-10-19 14:30:39 +0530

    Time: 385 ms (55.91%), Space: 209.9 MB (53.1%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,23 @@
 class Solution {
-    private:
-    int sus(int ind,int bd,int life,int n,vector<int>& prices, vector<vector<int>> & dp){
-        if(ind == n) return 0;
-        if(life == 0) return 0;
-        int profit  = 0;
-        if(dp[ind][bd] != -1) return dp[ind][bd];
-        if(bd){
-            profit = max(-prices[ind] + sus(ind+1,0,life,n,prices,dp),sus(ind+1,1,life,n,prices,dp));
-        }
-        else{
-            profit = max(prices[ind] + sus(ind+1,1,life-1,n,prices,dp), sus(ind+1,0,life,n,prices,dp));
-        }
-        return dp[ind][bd] = profit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return sus(0,1,3,n,prices,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+
+        for(int i= n-1; i >= 0; i--){
+            for(int bd = 0; bd < 2; bd++ ){
+                for(int life = 1; life < 3; life++){
+                    int profit  =0;
+                    if(bd){
+                        profit = max(-prices[i] + dp[i+1][0][life],dp[i+1][1][life]);
+                    }
+                    else{
+                        profit = max(prices[i] + dp[i+1][1][life-1],dp[i+1][0][life]);
+                    }
+                    dp[i][bd][life] = profit;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };

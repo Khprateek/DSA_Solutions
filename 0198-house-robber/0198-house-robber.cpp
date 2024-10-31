e52be1a2a6f64a9f65a38b030198ab74dfc0cb8b
Prateek kharwar <99474420+Khprateek@users.noreply.github.com>
2024-10-31 06:02:34 +0530

    Time: 0 ms (100%), Space: 9.5 MB (45.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,16 @@
 class Solution {
+    private:
+    int sus(int ind, vector<int> &nums,vector<int> &dp){
+        if(ind < 0) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int notpick = sus(ind-1,nums,dp);
+        int pick = nums[ind] + sus(ind-2,nums,dp);
+        return dp[ind] = max(pick,notpick);
+    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        int prev2 = 0;
-        int prev = nums[0];
-
-        for(int i = 1 ;i < n ; i++){
-            int take = nums[i]; if(i > 1) take += prev2;
-            int nottake = prev;
-
-            int curr = max(take, nottake);
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+        vector<int> dp(n,-1);
+        return sus(n-1,nums,dp);
     }
 };
